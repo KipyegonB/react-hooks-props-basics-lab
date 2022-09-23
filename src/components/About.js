2e0d4bbe5b93b7135d96ffb87a5a64a6d409f7e4
KipyegonB <koechbernard08@gmail.com>
2022-09-24 01:06:01 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  if(props.bio){
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2 style={{ color: "firebrick" }}>About Me</h2>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
-
+else{
+    <div id="about">
+      <h2 style={{ color: "firebrick" }}>About Me</h2>
+      </div>
+}
+}
 export default About;
